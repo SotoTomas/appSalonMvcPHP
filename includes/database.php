41fc98269546ddbec927de17a89121d2dfa1d84b
Soto Tomas Agustin <sototomas.nf@gmail.com>
2025-01-20 14:05:53 -0300

    Cambios para la versión 1.0.0  ==========================

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
 
 $db->set_charset('utf8');
 
